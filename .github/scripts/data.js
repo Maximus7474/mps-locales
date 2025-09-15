31335fe2bde4aac2f4d6ac328457ad50a4fe62f2
Maximus7474 <the.maximus.7474@gmail.com>
2025-09-15 22:52:23 +0200

    feat(scripts): static data

---
+++ .github/scripts/data.js
@@ -0,0 +1,17 @@
+const resources = [
+    { name: 'mps-tickets',   package: '6748250' },
+    { name: 'mps-notepad',   package: '6431472' },
+    { name: 'mps-fleecanow', package: '6947140' },
+];
+
+const localeLabels = {
+  'en': 'English (en)',
+  'de': 'German (de)',
+  'fr': 'French (fr)',
+  'es': 'Spanish (es)',
+  'it': 'Italian (it)',
+};
+
+module.exports = {
+    resources, localeLabels
+}
